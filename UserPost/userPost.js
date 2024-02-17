4150a4ea9425c33fbb720bb63ac649106c640e42
KgothatsoMasia <123masiakgothatso@gmail.com>
2024-02-17 14:41:25 +0200

    Update userPost.js and added scaling function

--- UserPost/userPost.js
+++ UserPost/userPost.js
@@ -6,6 +6,7 @@ import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
 import {faEllipsisH} from "@fortawesome/free-solid-svg-icons";
 import userPostStyle from "./userPostStyle";
 import {faBookmark, faHeart, faMessage} from "@fortawesome/free-regular-svg-icons";
+import {horizontalScale, scaleFonSize} from "../../assets/scaling";
 
 const UserPost = props => {
   return (
@@ -14,14 +15,14 @@ const UserPost = props => {
         <View style={userPostStyle.userContainer}>
           <UserProfileImage
             profileImage={props.profileImage}
-            imageDimensions={48}
+            imageDimensions={horizontalScale(48)}
           />
           <View style={userPostStyle.userTextContainer}>
             <Text style={userPostStyle.username}>{props.firstName} {props.lastName}</Text>
             {props.location && <Text style={userPostStyle.location}>{props.location}</Text>}
           </View>
         </View>
-        <FontAwesomeIcon icon={faEllipsisH} size={24} color={'#79869F'} />
+        <FontAwesomeIcon icon={faEllipsisH} size={scaleFonSize(24)} color={'#79869F'} />
       </View>
       <View style={userPostStyle.postImage}>
         <Image source={props.image} />
