127b944b8af189a2d33838f93101a5bcef583a14
KgothatsoMasia <123masiakgothatso@gmail.com>
2024-02-17 14:43:20 +0200

    Update userPostStyle.js and added scaling function

--- UserPost/userPostStyle.js
+++ UserPost/userPostStyle.js
@@ -1,9 +1,15 @@
-import {StyleSheet} from "react-native";
-import getFontFamily from "../../android/app/src/main/assets/fonts/helper";
+import {StyleSheet} from 'react-native';
+import getFontFamily from '../../android/app/src/main/assets/fonts/helper';
+import {
+  horizontalScale,
+  scaleFonSize,
+  verticalScale} from '../../assets/scaling';
 
 const userPostStyle = StyleSheet.create({
   userContainer: {flexDirection: 'row'},
-  userTextContainer: {justifyContent: 'center', marginLeft: 10},
+  userTextContainer: {
+    justifyContent: 'center',
+    marginLeft: horizontalScale(10)},
   user: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -13,31 +19,31 @@ const userPostStyle = StyleSheet.create({
     color: '#000',
     fontFamily: getFontFamily('bold'),
     fontWeight: '600',
-    fontSize: 16,
+    fontSize: scaleFonSize(16),
   },
   location: {
     color: '#79869F',
     fontFamily: getFontFamily('bold'),
     fontWeight: '400',
-    fontSize: 12,
-    marginTop: 5,
+    fontSize: scaleFonSize(12),
+    marginTop: verticalScale(5),
   },
   postImage: {
     alignItems: 'center',
-    marginVertical: 20,
+    marginVertical: verticalScale(20),
   },
   userPostContainer: {
-    marginTop: 35,
-    paddingBottom: 20,
+    marginTop: verticalScale(35),
+    paddingBottom: verticalScale(20),
     borderBottomWidth: 1,
     borderBottomColor: '#EFF2F6'
   },
-  userPostStats: {flexDirection: 'row', marginLeft: 10},
+  userPostStats: {flexDirection: 'row', marginLeft: horizontalScale(10)},
   heartIcon: {flexDirection: 'row'},
-  commentIcon: {flexDirection: 'row', marginLeft: 27},
-  bookmarkIcon: {flexDirection: 'row', marginLeft: 27},
+  commentIcon: {flexDirection: 'row', marginLeft: horizontalScale(27)},
+  bookmarkIcon: {flexDirection: 'row', marginLeft: horizontalScale(27)},
 
-  iconText: {marginLeft: 3, color: '#79869F'}
+  iconText: {marginLeft: horizontalScale(3), color: '#79869F'}
 });
 
 export default userPostStyle;
