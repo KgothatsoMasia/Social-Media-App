237224ee6fab7f94ac818b78488f16f40b114d82
KgothatsoMasia <123masiakgothatso@gmail.com>
2024-02-17 14:36:19 +0200

    Update Title.js

--- Title/Title.js
+++ Title/Title.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import {Text} from 'react-native';
 import PropTypes from 'prop-types';
+import Style from './style';
 
 const Title = props => {
-  return <Text style={
-      {fontFamily: 'Inter-Black',fontSize: 25,color:'#022150'}}>
-      {props.title}
-    </Text>;
+  return <Text style={Style.title}>{props.title}</Text>;
 };
 Title.propTypes = {
     title: PropTypes.string.isRequired,
