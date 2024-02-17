53e07abdfca18d5e86783691884146a910916f03
KgothatsoMasia <123masiakgothatso@gmail.com>
2024-02-17 14:44:58 +0200

    added scaling function

--- UserProfileImage/style.js
+++ UserProfileImage/style.js
@@ -1,10 +1,11 @@
 import {StyleSheet} from "react-native";
+import {horizontalScale} from "../../assets/scaling";
 
 const profileStyle = StyleSheet.create({
   userImageContainer: {
     borderColor: '#F35BAC',
-    padding: 4,
-    borderWidth: 2,
+    padding: horizontalScale(3),
+    borderWidth: 1,
   },
 });
 
